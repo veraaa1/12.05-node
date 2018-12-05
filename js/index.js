03c3be02ec3664cf4fb6830e27cbdd86bbc10a8a
veraaa1 <1301778436@qq.com>
2018-12-05 16:05:59 +0800

    add folder

---
+++ js/index.js
@@ -0,0 +1,9 @@
+const a = 10
+const b = require('./model.js')
+console.log(a, b)
+const $ = require('jquery')
+console.log($);
+const arr = [1, 2, 3]
+arr.each(function (ele) {
+    console.log(ele);
+})
---
+++ js/model.js
@@ -0,0 +1,2 @@
+const a = 50
+module.exports = a
